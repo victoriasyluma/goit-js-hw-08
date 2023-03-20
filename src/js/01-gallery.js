bc4a52d109d9c2206214fb792c1f691d338c00af
victoriasyluma <victoriasyluma3@gmail.com>
2023-03-20 16:05:06 +0100

    first step

---
+++ src/js/01-gallery.js
@@ -0,0 +1,24 @@
+import SimpleLightbox from 'simplelightbox';
+import { galleryItems } from './gallery-items';
+
+console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+let listElement = '';
+galleryItems.forEach((item) => {
+  const newImage = `<li class="gallery__item">
+   <a class="gallery__link" href="${item.original}">
+     <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+   </a>
+ </li>`;
+
+  listElement += newImage;
+});
+
+gallery.insertAdjacentHTML('afterbegin', listElement);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
